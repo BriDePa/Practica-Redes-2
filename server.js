b165617207834a66fb01c1cedcda176850b32363
BriDePa <deymarbrian02@gmail.com>
2025-07-20 13:58:38 -0400

    disenio de REST API

--- server.js
+++ server.js
@@ -4,7 +4,25 @@ const morgan = require("morgan");
 const app = express();
 app.use(morgan('dev'))
 
+app.get("/formulario", (req, res) => {
+    res.send('obteniendo formulario')
+})
 
+app.post("/formulario", (req, res) => {
+    res.send('creando formulario')
+})
+
+app.put("/formulario", (req, res) => {
+    res.send('actualizando formulario')
+})
+
+app.delete("/formulario", (req, res) => {
+    res.send('borrando formulario')
+})
+
+app.get("/formulario/:id", (req, res) => {
+    res.send('obteniendo un producto')
+})
 
 app.listen(3000);
 console.log(`servidor en puerto ${3000}`);
