dccd00b52cd31a0af6bbc09dac38eaaeaa03e0a1
BriDePa <deymarbrian02@gmail.com>
2025-07-20 17:31:26 -0400

    metodos HTTP

--- server.js
+++ server.js
@@ -1,28 +1,52 @@
 const express = require("express");
 const morgan = require("morgan");
+const path = require("path");
+require("ejs");
 
 const app = express();
-app.use(morgan('dev'))
+app.use(morgan("dev"));
+app.use(express.json());
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "static"));
 
 app.get("/formulario", (req, res) => {
-    res.send('obteniendo formulario')
+    res.send("metodo get")
 })
 
 app.post("/formulario", (req, res) => {
-    res.send('creando formulario')
+    res.send("metodo post")
 })
 
 app.put("/formulario", (req, res) => {
-    res.send('actualizando formulario')
+    res.send("metodo put")
 })
 
 app.delete("/formulario", (req, res) => {
-    res.send('borrando formulario')
+    res.send("metodo delete")
 })
 
-app.get("/formulario/:id", (req, res) => {
-    res.send('obteniendo un producto')
+app.patch("/formulario", (req, res) => {
+    res.send("metodo patch")
 })
+
+/* app.get("/about", (req, res) => {
+  const titulo = "pagina con express";
+  res.render("index.ejs", { titulo: titulo });
+}); */
+
+
+/* app.get("/formulario/:id", (req, res) => {
+  console.log(req.params.id);
+  const formEncontrado = forms.find((form) => form.id == req.params.id);
+
+  if (!formEncontrado)
+    return res.status(404).json({
+      mensaje: "no encontrado",
+    });
+  console.log(formEncontrado);
+
+  res.json(formEncontrado);
+}); */
 
 app.listen(3000);
 console.log(`servidor en puerto ${3000}`);
