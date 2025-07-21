a58a75297dfd97d589b60a09d6d9b8fa4448a4c1
BriDePa <deymarbrian02@gmail.com>
2025-07-20 21:20:02 -0400

    TEST de conexion

---
+++ test-db.js
@@ -0,0 +1,22 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "root",
+  host: "localhost",
+  database: "contactos_db",
+  password: "1201",
+  port: 5432,
+});
+
+async function test() {
+  try {
+    const contactos = await pool.query("SELECT * FROM contactos");
+    console.log("datos: " + contactos.rows);
+  } catch (error) {
+    console.log("error de conexion: " + error);
+  } finally {
+    await pool.end();
+  }
+}
+
+test();
